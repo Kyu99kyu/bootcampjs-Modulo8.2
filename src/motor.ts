986c0461562c4d9249cd5046e162d0fa9c25a609
sazo <sazdev09@gmail.com>
2024-05-26 19:39:52 +0200

    refactor: .Change constants name

--- src/motor.ts
+++ src/motor.ts
@@ -3,13 +3,13 @@ import "./style.css";
 
 //Apartado 1 
 //A) Queremos extraer la lista de paciente que están asignados a la especialidad de Pediatría
-const pediatra : string = "Pediatra";
-const edad : number = 10
+const ESPECIALIDAD_PEDIATRA : string = "Pediatra";
+const LIMITE_EDAD : number = 10
 
 export const obtenPacientesAsignadosAPediatria = (
     pacientes: Pacientes[]
   ): Pacientes[] => {
-    return pacientes.filter((paciente : Pacientes)=>paciente.especialidad === pediatra);
+    return pacientes.filter((paciente : Pacientes) : boolean=>paciente.especialidad === ESPECIALIDAD_PEDIATRA);
 };
 
 //B) Queremos extraer la lista de pacientes asignados a Pediatría y que tengan una edad menor de 10 años.
@@ -17,5 +17,5 @@ export const obtenPacientesAsignadosAPediatria = (
 export const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
     pacientes: Pacientes[]
   ): Pacientes[] => {
-    return pacientes.filter((paciente : Pacientes)=> paciente.especialidad === pediatra && paciente.edad < edad);
+    return pacientes.filter((paciente : Pacientes) : boolean => paciente.especialidad === ESPECIALIDAD_PEDIATRA && paciente.edad < LIMITE_EDAD);
 };

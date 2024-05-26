3add2cc0a3eb3b545fb428a8f817f1f42b23ca8e
sazo <sazdev09@gmail.com>
2024-05-26 20:02:53 +0200

    feat: . Add function activarProtocoloUrgencia

--- src/motor.ts
+++ src/motor.ts
@@ -4,7 +4,9 @@ import "./style.css";
 //Apartado 1 
 //A) Queremos extraer la lista de paciente que están asignados a la especialidad de Pediatría
 const ESPECIALIDAD_PEDIATRA : string = "Pediatra";
-const LIMITE_EDAD : number = 10
+const LIMITE_EDAD : number = 10;
+const FRECUENCIA_CARDIACA : number= 100;
+const TEMPERATURA : number = 39
 
 export const obtenPacientesAsignadosAPediatria = (
     pacientes: Pacientes[]
@@ -18,4 +20,17 @@ export const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
     pacientes: Pacientes[]
   ): Pacientes[] => {
     return pacientes.filter((paciente : Pacientes) : boolean => paciente.especialidad === ESPECIALIDAD_PEDIATRA && paciente.edad < LIMITE_EDAD);
+};
+
+//Apartado 2
+/**
+ Queremos activar el protocolo de urgencia si cualquier de los pacientes tiene un ritmo cardíaco superior a 100 pulsaciones por minuto y una temperatura corporal superior a 39 grados.
+
+Es decir, crear una función que devuelve true/false dependiendo si se da la condición, algo así como:
+ */
+
+export const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+    return pacientes.some((paciente : Pacientes): boolean =>
+         paciente.frecuenciaCardiaca > FRECUENCIA_CARDIACA &&
+         paciente.temperatura > TEMPERATURA)
 };
--- src/shell.ts
+++ src/shell.ts
@@ -1,9 +1,11 @@
-import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios} from "./motor";
+import {obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia} from "./motor";
 import {Pacientes,pacientes} from "./modelo";
 
 const listaDePacientes : Pacientes[] = obtenPacientesAsignadosAPediatria(pacientes);
 const pacientesPedriatriaMenorDeDiez : Pacientes[] = obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
+const protocoloUrgencia : boolean = activarProtocoloUrgencia(pacientes);
 
 
-console.log({listaDePacientes})
-console.log({pacientesPedriatriaMenorDeDiez})
+console.log({listaDePacientes});
+console.log({pacientesPedriatriaMenorDeDiez});
+console.log(protocoloUrgencia);
